9efe12296c8a8dbdb59e2fb0e17347b01ccf4e89
abin <sebumappu@gmail.com>
2026-06-08 17:45:07 +0530

    wip

--- lib/feature/game/presentation/widgets/game_keyboard.dart
+++ lib/feature/game/presentation/widgets/game_keyboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:production_ready_app/core/theme/app_colors.dart';
 import 'package:production_ready_app/feature/game/presentation/bloc/game_bloc.dart';
 
@@ -18,36 +19,115 @@ class GameKeyBoard extends StatelessWidget {
   Widget build(BuildContext context) {
     const rows = ['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM'];
 
-    return Container(
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [...rows.map((row) => _buildKeyBoard(context, state, row))],
-      ),
+    return BlocBuilder<GameBloc, GameState>(
+      builder: (context, state) {
+        return Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ...rows.map((row) => _buildKeyBoard(context, state, row)),
+            ],
+          ),
+        );
+      },
     );
   }
 
   Widget _buildKeyBoard(BuildContext context, GameState state, String row) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: []);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: row
+          .split('')
+          .map((key) => _buildKey(context, state, key))
+          .toList(),
+    );
   }
 
-  Widget _buildRow(BuildContext context, GameState state) {
-    return Row(mainAxisAlignment: MainAxisAlignment.start, children: []);
+  Widget _buildActionRow(BuildContext context, GameState state) {
+    var currentAttempt = state.currentAttempt ?? '';
+    var word = state.word ?? '';
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        
+        // delete button
+        Expanded(
+          child: AspectRatio(
+            aspectRatio: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(3),
+              child: ElevatedButton(
+                onPressed: () => onDelete(),
+
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                  backgroundColor: AppColors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(17),
+                  ),
+                ),
+                child: Icon(Icons.backspace, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+
+        // enter button
+        Expanded(
+          child: AspectRatio(
+            aspectRatio: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(3),
+              child: ElevatedButton(
+                onPressed: () => onDelete(),
+
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                  backgroundColor: AppColors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(17),
+                  ),
+                ),
+                child: Icon(Icons.backspace, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   Widget _buildKey(BuildContext context, GameState state, String key) {
-    return ElevatedButton(
-      onPressed: () {},
-      
-      style: ElevatedButton.styleFrom(
+    return Expanded(
+      child: AspectRatio(
+        aspectRatio: 0.8,
+        child: Padding(
+          padding: const EdgeInsets.all(3),
+          child: ElevatedButton(
+            onPressed: () => onKeyPressed(),
 
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.zero,
+              backgroundColor: getKeyColor(context, state, key),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(17),
+              ),
+            ),
+            child: Text(
+              key,
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: getTextColor(context, state, key),
+              ),
+            ),
+          ),
+        ),
       ),
-      child: Text(key,
-      style: TextStyle(),),
     );
   }
 
@@ -80,8 +160,6 @@ class GameKeyBoard extends StatelessWidget {
     }
     return Theme.of(context).colorScheme.onSurface;
   }
-
-
 
   Color getTextColor(BuildContext context, GameState state, String key) {
     final attempts = state.attempts ?? [];
